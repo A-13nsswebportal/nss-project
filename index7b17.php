519af481a8745909a3291ccd8c5d9ebaa10fe513
A-13nsswebportal <62325054+A-13nsswebportal@users.noreply.github.com>
2020-03-18 14:50:11 +0530

    Add files via upload

---
+++ index7b17.php
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<?xml-stylesheet href="/components/com_imageshow/assets/css/style.css" type="text/css"?>
+<feed xmlns="http://www.w3.org/2005/Atom"  xml:lang="en-gb">
+	<title type="text">Home || National Service Scheme </title>
+	<subtitle type="text">National Service Scheme is a student – centered programme and it is complementary to education. It is a noble experiment in academic extension. It inculcates the spirit of voluntary work among students and teachers through sustained community interaction. It brings our academic institutions closer to the society. It is a link between the campus and community, the college and village, knowledge and action.  The overall aim of NSS is the Personality Development of students through community service. It gives an extension dimension to Higher Education system and orients the student youth to community service.</subtitle>
+	<link rel="alternate" type="text/html" href="http://sritnss.org"/>
+	<id>http://sritnss.org/index.php</id>
+	<updated>2020-03-13T10:56:26+05:30</updated>
+	<generator uri="https://www.joomla.org">Joomla! - Open Source Content Management</generator>
+	<link rel="self" type="application/atom+xml" href="http://sritnss.org/index.php?format=feed&amp;type=atom"/>
+	<entry>
+		<title>Welcome to National Service Scheme </title>
+		<link rel="alternate" type="text/html" href="http://sritnss.org/index.php/47-template-details/general/15-Welcome%20to%20National%20Service%20Scheme%20"/>
+		<published>2011-03-31T08:55:00+05:30</published>
+		<updated>2011-03-31T08:55:00+05:30</updated>
+		<id>http://sritnss.org/index.php/47-template-details/general/15-Welcome%20to%20National%20Service%20Scheme%20</id>
+		<author>
+			<name>demoadmin</name>
+		</author>
+		<summary type="html">&lt;div class=&quot;feed-description&quot;&gt;&lt;div class=&quot;demo-frontpage&quot;&gt;
+&lt;div class=&quot;grid-layout template-feature clearafter&quot;&gt;
+&lt;div&gt;
+&lt;div class=&quot;jsn-template-featured&quot;&gt;&lt;img class=&quot;image-border&quot; style=&quot;display: block; margin-left: auto; margin-right: auto;&quot; src=&quot;http://sritnss.org/images/demo-img.jpg&quot; width=&quot;430&quot; height=&quot;287&quot; border=&quot;0&quot; /&gt;
+&lt;p style=&quot;text-align: justify;&quot;&gt;National Service Scheme is a student – centered programme and it is complementary to education. It is a noble experiment in academic extension. It inculcates the spirit of voluntary work among students and teachers through sustained community interaction. It brings our academic institutions closer to the society. It is a link between the campus and community, the college and village, knowledge and action.  The overall aim of NSS is the Personality Development of students through community service. It gives an extension dimension to Higher Education system and orients the student youth to community service.&lt;/p&gt;
+&lt;p style=&quot;text-align: justify;&quot;&gt;National Service Scheme, popularly known as NSS, was formally launched on 24th September, 1969, the birth centenary of the Father of the Nation. Starting with an enrolment of 40000 students in 37 Universities covering all states, the coverage of NSS students increased to about 3.8 Million (2016-17) 396 universities. 16331 college/Technical institutions. 11894 plus two students&lt;/p&gt;
+&lt;div align=&quot;center&quot;&gt; &lt;/div&gt;
+&lt;/div&gt;
+&lt;/div&gt;
+&lt;div&gt;
+&lt;div class=&quot;jsn-template-list&quot;&gt;
+&lt;div class=&quot;clearafter&quot;&gt;
+&lt;h3 class=&quot;jsn-moduletitle&quot;&gt;&lt;span class=&quot;subHead&quot;&gt;NSS Badge&lt;/span&gt;&lt;/h3&gt;
+&lt;a href=&quot;http://sritnss.org/index.php?option=com_content&amp;amp;view=article&amp;amp;id=5&amp;amp;Itemid=172&quot;&gt;&lt;img class=&quot;image-border image-left&quot; src=&quot;http://sritnss.org/images/demo-img1.png&quot; width=&quot;75&quot; border=&quot;0&quot; /&gt;&lt;/a&gt;
+&lt;p class=&quot;para&quot; style=&quot;text-align: justify;&quot;&gt;The NSS symbol is embossed on the NSS badge. The NSS volunteers wear it while undertaking any programme of community service. The Konark wheel in the symbol has eight bars which represent the 24 hours of the day. Hence, the badge reminds the wearer to be in readiness for service of the nation round the clock i.e. for 24 hours. The red colour in the badge indicates that the NSS volunteers are full of blood i.e. lively, active, energetic and full of high spirit. The navy blue colour indicates the cosmos of which the NSS is a tiny part, ready to contribute its share for the welfare of the mankind.&lt;/p&gt;
+&lt;/div&gt;
+&lt;div class=&quot;clearafter&quot;&gt;
+&lt;h3 class=&quot;jsn-moduletitle&quot;&gt;&lt;span class=&quot;subHead&quot;&gt;NSS Symbol&lt;/span&gt;&lt;/h3&gt;
+&lt;a href=&quot;http://sritnss.org/index.php?option=com_content&amp;amp;view=article&amp;amp;id=41&amp;amp;Itemid=174&quot;&gt;&lt;img class=&quot;image-border image-left&quot; src=&quot;http://sritnss.org/images/demo-img2.png&quot; width=&quot;75&quot; border=&quot;0&quot; /&gt;&lt;/a&gt;
+&lt;p class=&quot;para&quot; style=&quot;text-align: justify;&quot;&gt;The symbol of the National Service Scheme, as appearing on the cover page of this Manual is based on the ‘Rath’ wheel of the Konark Sun Temple situated in Orissa. These giant wheels of the Sun Temple portray the cycle of creation, preservation and release, and signify the movement in life across time and space. The design of the symbol, a simplified form of the Sun-chariot wheel primarily depicts movement. The wheel signifies the progressive cycle of life. It stands for continuity as well as change and implies the continuous striving of NSS for social transformation and upliftment.&lt;/p&gt;
+&lt;/div&gt;
+&lt;div class=&quot;clearafter&quot;&gt;
+&lt;h3 class=&quot;jsn-moduletitle&quot;&gt;&lt;a href=&quot;http://sritnss.org/index.php?option=com_content&amp;amp;view=article&amp;amp;id=3&amp;amp;Itemid=176&quot;&gt;NSS Song &lt;/a&gt;&lt;/h3&gt;
+&lt;a href=&quot;http://sritnss.org/index.php?option=com_content&amp;amp;view=article&amp;amp;id=3&amp;amp;Itemid=176&quot;&gt;&lt;img class=&quot;image-border image-left&quot; src=&quot;http://sritnss.org/images/demo-img3.png&quot; width=&quot;75&quot; border=&quot;0&quot; /&gt;&lt;/a&gt;
+&lt;p style=&quot;text-align: justify;&quot;&gt;During Silver Jubilee Year the NSS theme song has been composed. All NSS volunteers are expected to learn the theme song and sing the song during NSS programmes and celebrations. The theme song is available in download page.&lt;/p&gt;
+&lt;/div&gt;
+&lt;/div&gt;
+&lt;/div&gt;
+&lt;/div&gt;
+&lt;/div&gt;&lt;/div&gt;</summary>
+		<content type="html">&lt;div class=&quot;feed-description&quot;&gt;&lt;div class=&quot;demo-frontpage&quot;&gt;
+&lt;div class=&quot;grid-layout template-feature clearafter&quot;&gt;
+&lt;div&gt;
+&lt;div class=&quot;jsn-template-featured&quot;&gt;&lt;img class=&quot;image-border&quot; style=&quot;display: block; margin-left: auto; margin-right: auto;&quot; src=&quot;http://sritnss.org/images/demo-img.jpg&quot; width=&quot;430&quot; height=&quot;287&quot; border=&quot;0&quot; /&gt;
+&lt;p style=&quot;text-align: justify;&quot;&gt;National Service Scheme is a student – centered programme and it is complementary to education. It is a noble experiment in academic extension. It inculcates the spirit of voluntary work among students and teachers through sustained community interaction. It brings our academic institutions closer to the society. It is a link between the campus and community, the college and village, knowledge and action.  The overall aim of NSS is the Personality Development of students through community service. It gives an extension dimension to Higher Education system and orients the student youth to community service.&lt;/p&gt;
+&lt;p style=&quot;text-align: justify;&quot;&gt;National Service Scheme, popularly known as NSS, was formally launched on 24th September, 1969, the birth centenary of the Father of the Nation. Starting with an enrolment of 40000 students in 37 Universities covering all states, the coverage of NSS students increased to about 3.8 Million (2016-17) 396 universities. 16331 college/Technical institutions. 11894 plus two students&lt;/p&gt;
+&lt;div align=&quot;center&quot;&gt; &lt;/div&gt;
+&lt;/div&gt;
+&lt;/div&gt;
+&lt;div&gt;
+&lt;div class=&quot;jsn-template-list&quot;&gt;
+&lt;div class=&quot;clearafter&quot;&gt;
+&lt;h3 class=&quot;jsn-moduletitle&quot;&gt;&lt;span class=&quot;subHead&quot;&gt;NSS Badge&lt;/span&gt;&lt;/h3&gt;
+&lt;a href=&quot;http://sritnss.org/index.php?option=com_content&amp;amp;view=article&amp;amp;id=5&amp;amp;Itemid=172&quot;&gt;&lt;img class=&quot;image-border image-left&quot; src=&quot;http://sritnss.org/images/demo-img1.png&quot; width=&quot;75&quot; border=&quot;0&quot; /&gt;&lt;/a&gt;
+&lt;p class=&quot;para&quot; style=&quot;text-align: justify;&quot;&gt;The NSS symbol is embossed on the NSS badge. The NSS volunteers wear it while undertaking any programme of community service. The Konark wheel in the symbol has eight bars which represent the 24 hours of the day. Hence, the badge reminds the wearer to be in readiness for service of the nation round the clock i.e. for 24 hours. The red colour in the badge indicates that the NSS volunteers are full of blood i.e. lively, active, energetic and full of high spirit. The navy blue colour indicates the cosmos of which the NSS is a tiny part, ready to contribute its share for the welfare of the mankind.&lt;/p&gt;
+&lt;/div&gt;
+&lt;div class=&quot;clearafter&quot;&gt;
+&lt;h3 class=&quot;jsn-moduletitle&quot;&gt;&lt;span class=&quot;subHead&quot;&gt;NSS Symbol&lt;/span&gt;&lt;/h3&gt;
+&lt;a href=&quot;http://sritnss.org/index.php?option=com_content&amp;amp;view=article&amp;amp;id=41&amp;amp;Itemid=174&quot;&gt;&lt;img class=&quot;image-border image-left&quot; src=&quot;http://sritnss.org/images/demo-img2.png&quot; width=&quot;75&quot; border=&quot;0&quot; /&gt;&lt;/a&gt;
+&lt;p class=&quot;para&quot; style=&quot;text-align: justify;&quot;&gt;The symbol of the National Service Scheme, as appearing on the cover page of this Manual is based on the ‘Rath’ wheel of the Konark Sun Temple situated in Orissa. These giant wheels of the Sun Temple portray the cycle of creation, preservation and release, and signify the movement in life across time and space. The design of the symbol, a simplified form of the Sun-chariot wheel primarily depicts movement. The wheel signifies the progressive cycle of life. It stands for continuity as well as change and implies the continuous striving of NSS for social transformation and upliftment.&lt;/p&gt;
+&lt;/div&gt;
+&lt;div class=&quot;clearafter&quot;&gt;
+&lt;h3 class=&quot;jsn-moduletitle&quot;&gt;&lt;a href=&quot;http://sritnss.org/index.php?option=com_content&amp;amp;view=article&amp;amp;id=3&amp;amp;Itemid=176&quot;&gt;NSS Song &lt;/a&gt;&lt;/h3&gt;
+&lt;a href=&quot;http://sritnss.org/index.php?option=com_content&amp;amp;view=article&amp;amp;id=3&amp;amp;Itemid=176&quot;&gt;&lt;img class=&quot;image-border image-left&quot; src=&quot;http://sritnss.org/images/demo-img3.png&quot; width=&quot;75&quot; border=&quot;0&quot; /&gt;&lt;/a&gt;
+&lt;p style=&quot;text-align: justify;&quot;&gt;During Silver Jubilee Year the NSS theme song has been composed. All NSS volunteers are expected to learn the theme song and sing the song during NSS programmes and celebrations. The theme song is available in download page.&lt;/p&gt;
+&lt;/div&gt;
+&lt;/div&gt;
+&lt;/div&gt;
+&lt;/div&gt;
+&lt;/div&gt;&lt;/div&gt;</content>
+		<category term="Featured" />
+		<category term="General" />
+		<category term="Template Details" />
+	</entry>
+</feed>
